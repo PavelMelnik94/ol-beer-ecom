41299f1c0d328965d7f49f506c6813467879b7a8
Pavel Melnik <recyclesordie94@gmail.com>
2025-06-28 20:07:49 +0300

    feat(home/breweries): add responsive hero sections with illustrations
    refactor(hero): implement responsive layout using react-responsive
    style(hero): add image container styles for mobile view
    chore: add react-responsive dependency
    docs: add new illustrations for hero sections

--- src/pages/breweries/ui/hero.tsx
+++ src/pages/breweries/ui/hero.tsx
@@ -1,18 +1,27 @@
-import { useGoTo } from '@kernel/index';
-import { Button, Flex, Heading, Section } from '@radix-ui/themes';
+import { Box, Flex, Heading, Section } from '@radix-ui/themes';
+import { Image } from '@shared/components';
+import clsx from 'clsx';
+import { useMediaQuery } from 'react-responsive'
+import styles from './hero.module.scss';
 
 export function Hero() {
-  const { navigateToStore } = useGoTo()
-
+  const isColumnDirection = useMediaQuery({
+    query: '(max-width: 1100px)',
+  })
   return (
     <Section>
-      <Heading mb="2" size="9" align="center">Brewery Insights & Inspirations</Heading>
-      <Heading mb="3" size="7" align="center">Step inside iconic breweries, discover their unique stories, and access exclusive tours and recommendations designed to enrich your beer journey.</Heading>
-      <Flex justify="center" align="center">
-        <Button size="2" onClick={navigateToStore}>
-          Explore Now
-        </Button>
+      <Flex gap="2" direction={isColumnDirection ? 'column' : 'row'}>
+        <Box>
+          <Heading mb="2" size="9" align="center">Brewery Insights & Inspirations</Heading>
+          <Heading mb="3" size="7" align="center">Step inside iconic breweries, discover their unique stories, and access exclusive tours and recommendations designed to enrich your beer journey.</Heading>
+        </Box>
+        <Image
+          containerClassName={clsx({ [styles.imgContainer]: isColumnDirection })}
+          src="/illustrations/u_company.svg"
+          alt="have a fun"
+        />
       </Flex>
+
     </Section>
   )
 }
--- src/pages/home/ui/hero.tsx
+++ src/pages/home/ui/hero.tsx
@@ -1,10 +1,27 @@
-import { Button, Flex, Heading, Section } from '@radix-ui/themes';
+import { Box, Button, Flex, Heading, Section } from '@radix-ui/themes';
+import { Image, Show } from '@shared/components';
+import { useMediaQuery } from 'react-responsive';
 
 export function Hero() {
+  const isColumnDirection = useMediaQuery({
+    query: '(max-width: 1100px)',
+  })
+
   return (
     <Section>
-      <Heading mb="2" size="9" align="center">Your Guide to Liquid Gold</Heading>
-      <Heading mb="3" size="7" align="center">From Belgian abbeys to American craft breweries, explore curated reviews, brewing insights, and exclusive marketplace finds that elevate your beer experience.</Heading>
+      <Flex gap="2" direction={isColumnDirection ? 'column' : 'row'}>
+        <Box>
+          <Heading mb="2" size="9" align="center">Your Guide to Liquid Gold</Heading>
+          <Heading mb="3" size="7" align="center">From Belgian abbeys to American craft breweries, explore curated reviews, brewing insights, and exclusive marketplace finds that elevate your beer experience.</Heading>
+        </Box>
+        <Show when={!isColumnDirection}>
+          <Image
+            src="/illustrations/u_beer.svg"
+            alt="have a fun"
+          />
+        </Show>
+
+      </Flex>
       <Flex justify="center" align="center">
         <Button size="2">
           Explore Now
