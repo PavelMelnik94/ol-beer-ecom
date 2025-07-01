d1d8a5c925183b33e75003282378532b4c306d3e
Pavel Melnik <recyclesordie94@gmail.com>
2025-07-01 14:54:14 +0300

    refactor(auth-store): wrap auth store with devtools middleware

--- src/modules/auth/stores/auth-store.ts
+++ src/modules/auth/stores/auth-store.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { devtools, persist } from 'zustand/middleware'
 import type { User } from '@kernel/index';
 import { tokenStorage } from '@kernel/index'
 import type { AuthState } from '../model/types'
@@ -11,37 +11,39 @@ interface AuthStore extends AuthState {
 }
 
 export const useAuthStore = create<AuthStore>()(
-  persist(
-    set => ({
-      user: null,
-      isAuth: false,
+  devtools(
+    persist(
+      set => ({
+        user: null,
+        isAuth: false,
 
-      setUser: user =>
-        set({ user, isAuth: !!user }),
+        setUser: user =>
+          set({ user, isAuth: !!user }),
 
-      login: (user) => {
-        tokenStorage.set(user.token)
-        set({
-          user,
-          isAuth: true,
-        })
-      },
+        login: (user) => {
+          tokenStorage.set(user.token)
+          set({
+            user,
+            isAuth: true,
+          })
+        },
 
-      logout: () => {
-        tokenStorage.remove()
-        set({
-          user: null,
-          isAuth: false,
-        })
-      },
+        logout: () => {
+          tokenStorage.remove()
+          set({
+            user: null,
+            isAuth: false,
+          })
+        },
 
-    }),
-    {
-      name: 'auth-storage',
-      partialize: state => ({
-        user: state.user,
-        isAuth: state.isAuth,
       }),
-    },
+      {
+        name: 'auth-storage',
+        partialize: state => ({
+          user: state.user,
+          isAuth: state.isAuth,
+        }),
+      },
+    ),
   ),
 )
