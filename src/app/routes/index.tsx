166abb7449ca1a88ca9b1da585761cd57722c27e
Pavel Melnik <recyclesordie94@gmail.com>
2025-07-07 02:21:32 +0300

    refactor: consolidate lazy loading exports for improved code organization

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,17 +1,11 @@
 import { AuthLayout } from '@app/layouts/auth-layout/auth-layout';
 import { MainLayout } from '@app/layouts/main-layout/main-layout';
 import { RouteErrorBoundary, ROUTES } from '@kernel/index';
-import { HomePage } from '@pages/index';
+import { HomePage, LazyArticlePage, LazyBlogPage, LazyBreweriesPage, LazyProductDetailsPage, LazyProductsPage } from '@pages/index';
 import { PagePreloader } from '@shared/components';
-import { lazy, Suspense } from 'react';
+import { Suspense } from 'react';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-const LazyArticlePage = lazy(() => import('./../../pages').then(module => ({ default: module.ArticlePage })));
-const LazyBreweriesPage = lazy(() => import('./../../pages').then(module => ({ default: module.BreweriesPage })));
-const LazyBlogPage = lazy(() => import('./../../pages').then(module => ({ default: module.BlogPage })));
-const LazyProductsPage = lazy(() => import('./../../pages').then(module => ({ default: module.ProductsPage })));
-const LazyProductDetailsPage = lazy(() => import('./../../pages').then(module => ({ default: module.ProductDetailsPage })));
 
 const router = createBrowserRouter([
   {
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,22 @@
+import { lazy } from 'react';
+
 export { ArticlePage } from './article/article-page';
 export { BlogPage } from './blog/blog-page';
 export { BreweriesPage } from './breweries/breweries-page';
 export { HomePage } from './home/home-page';
 export { ProductDetailsPage } from './product-details/product-details-page';
 export { ProductsPage } from './products/products-page';
+
+const LazyArticlePage = lazy(() => import('./article/article-page').then(module => ({ default: module.ArticlePage })));
+const LazyBlogPage = lazy(() => import('./blog/blog-page').then(module => ({ default: module.BlogPage })));
+const LazyBreweriesPage = lazy(() => import('./breweries/breweries-page').then(module => ({ default: module.BreweriesPage })));
+const LazyProductsPage = lazy(() => import('./products/products-page').then(module => ({ default: module.ProductsPage })));
+const LazyProductDetailsPage = lazy(() => import('./product-details/product-details-page').then(module => ({ default: module.ProductDetailsPage })));
+
+export {
+  LazyArticlePage,
+  LazyBlogPage,
+  LazyBreweriesPage,
+  LazyProductDetailsPage,
+  LazyProductsPage,
+};
