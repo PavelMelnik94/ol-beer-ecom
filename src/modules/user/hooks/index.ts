782d505b179d323bb6fe1c496f7ab8e2dd8b6677
Pavel Melnik <recyclesordie94@gmail.com>
2025-07-11 12:13:24 +0300

    feat: add user hooks for address, avatar, billing, shipping, favorites, and ratings management

---
+++ src/modules/user/hooks/index.ts
@@ -0,0 +1,35 @@
+// Addresses
+export {
+  useCreateAddress,
+  useDeleteAddress,
+  useSetAddressPrimary,
+  useUpdateAddress,
+  useUserAddress,
+  useUserAddresses,
+} from './use-user-addresses';
+
+// Avatar
+export {
+  useDeleteAvatar,
+  useUploadAvatar,
+  useUserAvatar,
+} from './use-user-avatar';
+
+// Billing and Shipping
+export {
+  useUserBillingAddresses,
+  useUserShippingAddresses,
+} from './use-user-billing-shipping';
+
+// Favorites
+export {
+  useIsFavorite,
+  useToggleFavorite,
+  useUserFavorites,
+} from './use-user-favorites';
+
+// Ratings
+export {
+  useUserRating,
+  useUserRatings,
+} from './use-user-ratings';
---
+++ src/modules/user/index.ts
@@ -0,0 +1,8 @@
+// Hooks
+export * from './hooks';
+
+// Model
+export * from './model';
+
+// Store
+export { userStore } from './stores/user-store';
