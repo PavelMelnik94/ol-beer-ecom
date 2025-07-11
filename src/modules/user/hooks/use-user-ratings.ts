3d756e655faa39017e03b9f85491d7743859c222
Pavel Melnik <recyclesordie94@gmail.com>
2025-07-11 12:13:17 +0300

    feat: add hooks for fetching user ratings and specific product ratings

---
+++ src/modules/user/hooks/use-user-ratings.ts
@@ -0,0 +1,30 @@
+import type { SuccessResponseRatings } from '@modules/user/api';
+import type { ErrorResponse } from 'react-router-dom';
+import { QUERY_KEYS } from '@kernel/query';
+import { userApi } from '@modules/user/api';
+import { userStore } from '@modules/user/stores/user-store';
+import { useQuery } from '@tanstack/react-query';
+
+export function useUserRatings() {
+  const { data: response, error, isLoading } = useQuery<SuccessResponseRatings, ErrorResponse>({
+    queryKey: QUERY_KEYS.user.ratings(),
+    queryFn: () => userApi.getRatings(),
+  });
+
+  return {
+    ratings: response?.data || [],
+    isLoading,
+    error,
+  } as const;
+}
+
+export function useUserRating(productId: string) {
+  const { hasRating } = userStore();
+
+  const rating = hasRating(productId);
+
+  return {
+    rating,
+    hasRating: !!rating,
+  } as const;
+}
