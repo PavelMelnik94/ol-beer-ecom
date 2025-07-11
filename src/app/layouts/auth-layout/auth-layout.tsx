154c0ff87f30a6f90fbe5b92e6ccde2205bec149
Pavel Melnik <recyclesordie94@gmail.com>
2025-07-12 00:37:42 +0300

    refactor: update AuthLayout structure and styles for improved scroll handling

--- src/app/layouts/auth-layout/auth-layout.tsx
+++ src/app/layouts/auth-layout/auth-layout.tsx
@@ -1,12 +1,24 @@
+import { useGlobalScroll } from '@kernel/hooks';
+import { Header } from '@modules/common';
+import { Box } from '@radix-ui/themes';
+import { Footer } from '@shared/components';
+import clsx from 'clsx';
 import { Outlet } from 'react-router-dom';
 import styles from './auth-layout.module.scss';
 
 export function AuthLayout() {
+  const { scrollY } = useGlobalScroll();
+
+  const isFixedHeader = scrollY > 3;
+
   return (
-    <div className={styles.layout}>
-      <div className={styles.container}>
+    <div className={clsx(styles.layout, { [styles.layoutWithScrollActive]: isFixedHeader })}>
+      <Header isFixed={isFixedHeader} />
+      <Box className={styles.decorativeBg} />
+      <main className={styles.main}>
         <Outlet />
-      </div>
+      </main>
+      <Footer />
     </div>
   );
 }
