1f7cf74ed0317ccf97cc29796d4598ba12cc427a
Pavel Melnik <recyclesordie94@gmail.com>
2025-07-12 02:35:19 +0300

    feat: restructure register store; define interfaces for personal info, address, and security info

--- src/modules/auth/stores/register-store.ts
+++ src/modules/auth/stores/register-store.ts
@@ -1,16 +1,60 @@
 import { create } from 'zustand';
 import { subscribeWithSelector } from 'zustand/middleware';
 
-interface State {
-
+export interface PersonalInfo {
+  firstName: string;
+  lastName: string;
+  email: string;
 }
 
-interface Actions {
-
+export interface Address {
+  city: string;
+  country: string;
+  streetName: string;
+  zip: string;
+  type: 'billing' | 'shipping';
+  isPrimaryAddress?: boolean;
 }
 
-export const useRegisterStore = create<State & Actions>()(
-  subscribeWithSelector((set, get) => ({
+export interface SecurityInfo {
+  password: string;
+  confirmPassword: string;
+}
 
+export interface RegisterFormState {
+  step: number;
+  personalInfo: PersonalInfo;
+  addresses: Address[];
+  security: SecurityInfo;
+  setStep: (step: number) => void;
+  setPersonalInfo: (data: Partial<PersonalInfo>) => void;
+  setAddresses: (addresses: Address[]) => void;
+  setSecurity: (data: Partial<SecurityInfo>) => void;
+  reset: () => void;
+}
+
+export const useRegisterStore = create<RegisterFormState>()(
+  subscribeWithSelector(set => ({
+    step: 1,
+    personalInfo: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
+    addresses: [],
+    security: {
+      password: '',
+      confirmPassword: '',
+    },
+    setStep: step => set({ step }),
+    setPersonalInfo: data => set(state => ({ personalInfo: { ...state.personalInfo, ...data } })),
+    setAddresses: addresses => set({ addresses }),
+    setSecurity: data => set(state => ({ security: { ...state.security, ...data } })),
+    reset: () => set({
+      step: 1,
+      personalInfo: { firstName: '', lastName: '', email: '' },
+      addresses: [],
+      security: { password: '', confirmPassword: '' },
+    }),
   })),
 );
