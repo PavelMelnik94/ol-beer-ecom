636ed1fd466c6ece0315109cdbd8999f828ebdcd
Pavel Melnik <recyclesordie94@gmail.com>
2025-07-12 03:45:33 +0300

    feat: update ZIP code validation in AddressSchema to allow only numeric values

--- src/modules/auth/model/schema/index.ts
+++ src/modules/auth/model/schema/index.ts
@@ -6,7 +6,7 @@ export const AddressSchema = z.object({
 city: z.string().min(1, 'City is required').regex(/^[A-Za-z\s'-]+$/, 'Only latin letters allowed'),
 country: z.string().min(1, 'Country is required').regex(/^[A-Za-z\s'-]+$/, 'Only latin letters allowed'),
 streetName: z.string().min(1, 'Street name is required').regex(/^[A-Za-z\s'-]+$/, 'Only latin letters allowed'),
-zip: z.string().min(1, 'ZIP code is required').regex(/^[A-Za-z0-9\s-]+$/, 'Only latin letters and numbers allowed'),
+zip: z.string().min(1, 'ZIP code is required').regex(/^[0-9]+$/, 'Only numbers allowed'),
   type: z.enum(['billing', 'shipping']),
   isPrimaryAddress: z.boolean().default(true),
 });
